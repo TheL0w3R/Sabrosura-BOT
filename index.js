ac7bfee1d588d284cbae86eab930a1524eae2d37
TheL0w3R <thel0w3r97@gmail.com>
2020-09-12 16:08:05 +0100

    Fixing error where the user requesting the bot is not in a voice channel

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ bot.on('message', message => {
     if (isReady && message.content === 'sabrosura') {
         isReady = false;
         var voiceChannel = message.member.voice.channel;
-        voiceChannel.join().then(connection => {
-            const dispatcher = connection.play(fs.createReadStream('./media/sabrosura_short.opus'), { type: 'ogg/opus' });
-            dispatcher.on("finish", () => {
-                voiceChannel.leave();
-            });
-        }).catch(err => console.log(err));
+        if (voiceChannel !== null && voiceChannel !== undefined) {
+            voiceChannel.join().then(connection => {
+                const dispatcher = connection.play(fs.createReadStream('./media/sabrosura_short.opus'), { type: 'ogg/opus' });
+                dispatcher.on("finish", () => {
+                    voiceChannel.leave();
+                });
+            }).catch(err => console.log(err));
+        }
         isReady = true;
     }
 });
