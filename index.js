afad7e8281f5f57b9926e1ebfc6f16abddcb4bd8
TheL0w3R <thel0w3r97@gmail.com>
2020-09-12 15:54:40 +0100

    Init commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var Discord = require('discord.js');
+var bot = new Discord.Client();
+var isReady = true;
+const fs = require('fs');
+
+bot.on('debug', console.log)
+
+bot.on('message', message => {
+    if (isReady && message.content === 'sabrosura') {
+        isReady = false;
+        var voiceChannel = message.member.voice.channel;
+        voiceChannel.join().then(connection => {
+            const dispatcher = connection.play(fs.createReadStream('./media/sabrosura_short.opus'), { type: 'ogg/opus' });
+            dispatcher.on("finish", () => {
+                voiceChannel.leave();
+            });
+        }).catch(err => console.log(err));
+        isReady = true;
+    }
+});
+
+
+bot.login(process.env.SABROSURA_TOKEN);
